313ac01bed18a6d399bbebfcca808e6293e0333f
Diego Campo <xmariachi@gmail.com>
2024-04-03 22:09:23 +0200

    Fix topicId issue, bulletproof prev losses

--- main.py
+++ main.py
@@ -7,21 +7,36 @@ import json
 ETHUSD_TOKEN = "ETHUSD"
 TRUTH_ADDRESS = os.environ['TRUTH_API_ADDRESS']
 
-def process(token_name, timestamp):
-    url = f"{TRUTH_ADDRESS}/get/{token_name}/{timestamp}"
+def get_ground_truth(token_name, timestamp):
+    url = f"{TRUTH_ADDRESS}/gt/{token_name}/{timestamp}"
     response = requests.get(url)
     return response.text
-    
+
+def get_previous_losses(topic):
+    url = f"{TRUTH_ADDRESS}/losses/{topic}"
+    response = requests.get(url)
+    return response.text
 
 if __name__ == "__main__":
     # Your code logic with the parsed argument goes here
     try:
         # Not using to discriminate by topicId for simplicity.
-        # topic_id = sys.argv[1]
+        topic_id = sys.argv[1]
         if len(sys.argv) >= 3:
             timestamp = sys.argv[2]
-        response = process(token_name=ETHUSD_TOKEN, timestamp=timestamp)
-
+        response_gt = get_ground_truth(token_name=ETHUSD_TOKEN, timestamp=timestamp)
+        json_gt = json.loads(response_gt)
+        try:
+            response_losses = get_previous_losses(topic=topic_id)
+            json_losses = json.loads(response_losses)
+            response_dict = {"gt": json_gt, "losses": json_losses}
+            # Serialize as JSON
+            value = json.dumps(response_dict)
+        except Exception as e:
+            # Print only ground truth if losses are not available
+            response_dict = {"gt": json_gt}
+            value = json.dumps(response_dict)
     except Exception as e:
-        response = json.dumps({"error": {str(e)}})
-    print(response)
+        # No gt, no value - error
+        value = json.dumps({"error": {str(e)}})
+    print(value)
